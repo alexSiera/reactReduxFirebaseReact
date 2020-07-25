e529625697a73bf4f0585fd1d2d0400947de32da
alexanderPavlovtsev <alexander.pavlovtsev@fusion-team.com>
2020-07-25 23:56:53 +0300

    setup firestore firebase

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from './config/fbConfig';
+const store = createStore(
+  rootReducer,
+    composeWithDevTools(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
+    )
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
