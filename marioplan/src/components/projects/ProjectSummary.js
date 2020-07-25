4252fe4193e2cfda9fb49b8c3944ddfd22b76154
alexanderPavlovtsev <alexander.pavlovtsev@fusion-team.com>
2020-07-25 19:04:26 +0300

    add projectDetails component

--- marioplan/src/components/projects/ProjectSummary.js
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -2,10 +2,10 @@ import React from 'react';
 const ProjectSummary = () => {
   return (
     <div className='card z-depth-0 project-summary'>
-        <div className='card-content gray-text text-darken-3'>
+        <div className='card-content grey-text text-darken-3'>
           <span className='card-title'>Project title</span>
           <p>Posted by the Net Ninja</p>
-          <p className='gray-text'>3rd September, 2am</p>
+          <p className='grey-text'>3rd September, 2am</p>
         </div>
       </div>
   )
