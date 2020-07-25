78f4889844e4a7a389c0670f2ede21824cdf2269
alexanderPavlovtsev <alexander.pavlovtsev@fusion-team.com>
2020-07-25 22:52:18 +0300

    next up

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+var config = {
+  apiKey: "AIzaSyA6eBijos-nj6OX5yBakXBmbUeYqGRDIm8",
+  authDomain: "net-ninja-marioplan-c17df.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-c17df.firebaseio.com",
+  projectId: "net-ninja-marioplan-c17df",
+  storageBucket: "net-ninja-marioplan-c17df.appspot.com",
+  messagingSenderId: "680369265227",
+  appId: "1:680369265227:web:a400d9054118751c4a0bce",
+  measurementId: "G-7LJPTHXWVH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({timestampsInSnapshots: true})
+export default firebase;
