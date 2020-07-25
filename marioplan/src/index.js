fd02480aef511e102ee9926ebdc31408d425ec2b
alexanderPavlovtsev <alexander.pavlovtsev@fusion-team.com>
2020-07-25 22:13:59 +0300

    connect thunk

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux'
-const store = createStore(rootReducer);
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,5 +1,15 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'help me find peach', content: 'blah blah blah'},
+    {id: '2', title: 'Booom in reality', content: 'Oh no oh no oh no'},
+    {id: '3', title: 'Bird is crashing', content: 'blah blah blah'}
+  ]
+}
 const projectReducer = (state = initState, action) => {
+  switch(action.type) {
+    case "CREATE_PROJECT" : 
+      console.log('created project', action.project);
+  }
   return state
 }
 export default projectReducer;
